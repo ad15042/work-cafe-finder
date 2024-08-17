852a9a51499d6d935181bb75e84cc5151e1acf20
ad15042 <yudai.onep@gmail.com>
2024-08-17 17:48:42 +0900

    エラー処理の追加 ・クライアントサイドのフォームバリデーションの追加 ・サーバサイドでのエラーハンドリングの追加 ・エラー用のテンプレート画面を作成 ・JOIを使用したスキーマバリデーションの追加

---
+++ schema.js
@@ -0,0 +1,18 @@
+// joi
+const Joi = require('joi');
+
+// スキーマを定義
+module.exports.cafeSchema = Joi.object({
+    cafeData: Joi.object({ // リクエストボディにcafeDataがあることを定義
+        //　cafeDataの中身がどういう値かをobjectの中に定義する
+        shopName: Joi.string().required(), // String&必須項目
+        priceOfcoffee: Joi.number().min(0).required(), // 数値型&必須項目&最小値は0
+        location: Joi.string().required(), // String&必須項目
+        hasWiFi: Joi.boolean().required(), // Boolean&必須項目
+        wifiStrength: Joi.number().min(0).max(100).required(), // 数値&必須項目&最小値０&最大値100
+        confort: Joi.number().min(0).max(10).required(), // 数値&必須項目&最小値０&最大値10
+        comment: Joi.string(), // String
+        registerDate: Joi.string(), // String
+        image: Joi.string(), // String
+    }).required() // リクエストボディのcafeDataは必須項目であることを定義
+});
